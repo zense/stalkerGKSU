438d88073b7c8140a0c221c218a9289251b37a65
Gopalakrishnan.V <gopuvenkat@yahoo.com>
2017-09-06 17:48:06 +0530

    loading image added

--- static/utility.js
+++ static/utility.js
@@ -1,3 +1,18 @@
 function alert_query_will_be_submitted(){
   alert('Your query will be submitted. Please press OK to confirm and continue');
 }
+
+function loading(){
+    try{
+      var x = document.getElementById('container');
+      var y = document.getElementById('loading');
+      if(x.style.display == 'none') {
+        x.style.display = 'block';
+      } else {
+        y.style.display = 'block';
+        x.style.display = 'none';
+      }
+    } catch(e) {
+      console.log("nope :(");
+    }
+}
