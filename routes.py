1bea5ce7dec6abfb8041838495cfb855832582bf
Gopalakrishnan.V <gopuvenkat@yahoo.com>
2017-12-15 01:16:30 +0530

    update 1. send confirmation email 2. set threshold

--- routes.py
+++ routes.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import get_nodes
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
+from rq import Queue
+from rq.job import Job
+from worker import conn
+import uuid
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = "some_secret"
-
 db = SQLAlchemy(app)
+mail = Mail(app)
 
+q = Queue(connection=conn)
+
+import emails
 from models import *
 
 @app.route("/")
@@ -17,26 +26,35 @@ from models import *
 def home():
 	return render_template('home.html')
 
+def save_info(organisation, email_address):
+	key = str(uuid.uuid4())
+	items = get_nodes.main(organisation)
+	for i in items:
+		usr = User(organisation,i.name,i.github_username)
+		db.session.add(usr)
+	db.session.commit()
+	emails.send_email(organisation, [email_address])
+	return key
+
 @app.route("/query", methods = ['GET', 'POST'])
 def query():
-	if(request.method == 'GET'):
-		return render_template('query.html')
-	else:
+	if(request.method == 'POST'):
 		organisation = request.form['organisation']
+		email_address = request.form['email_address']
 		filename = organisation + ".html"
 		info = db.session.query(User.github_username, User.name).filter_by(organisation = organisation).all()
 		if(info == []):
-			items = get_nodes.main(organisation)
-			for i in items:
-				usr = User(organisation,i.name,i.github_username)
-				db.session.add(usr)
-			db.session.commit()
+			job = q.enqueue_call(
+				func="routes.save_info", args=(organisation, email_address, ), result_ttl=5000
+			)
+			flash("We shall notify you at " + email_address + " when the processing is complete")
 		else:
 			lists = []
 			for i in info:
 				lists.append([str(i.github_username), str(i.name)])
 			get_nodes.creating_objs(lists, organisation)
-		return render_template(filename, organisation = str(organisation) + '.json')
+			return render_template(filename, organisation=str(organisation)+'.json')
+	return render_template('query.html')
 
 @app.route("/aboutus")
 def contactus():
